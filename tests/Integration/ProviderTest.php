0184cfd6baa1dbb24f782cd76e360c922abab172
Giovani Patrick <giovani.ks@hotmail.com>
2026-09-24 19:14:26 -0300

    test(serializer): cover response formats

--- tests/Integration/ProviderTest.php
+++ tests/Integration/ProviderTest.php
@@ -6,6 +6,9 @@ namespace Xtream\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 use Xtream\Client;
+use Xtream\Serializer\CamelCaseSerializer;
+use Xtream\Serializer\JsonApiSerializer;
+use Xtream\Serializer\StandardizedSerializer;
 
 final class ProviderTest extends TestCase
 {
@@ -30,5 +33,39 @@ final class ProviderTest extends TestCase
         self::assertSame('array', gettype($client->getChannelCategories()));
         self::assertSame('array', gettype($client->getMovieCategories()));
         self::assertSame('array', gettype($client->getShowCategories()));
+    }
+
+    public function testProviderProfilesCanBeSerialized()
+    {
+        $baseUrl = getenv('XTREAM_BASE_URL');
+        $username = getenv('XTREAM_USERNAME');
+        $password = getenv('XTREAM_PASSWORD');
+
+        if (!$baseUrl || !$username || !$password) {
+            self::markTestSkipped('Live provider environment variables are not configured.');
+        }
+
+        $camelClient = new Client([
+            'url' => $baseUrl,
+            'username' => $username,
+            'password' => $password,
+            'serializer' => new CamelCaseSerializer(),
+        ]);
+        $standardClient = new Client([
+            'url' => $baseUrl,
+            'username' => $username,
+            'password' => $password,
+            'serializer' => new StandardizedSerializer(),
+        ]);
+        $jsonApiClient = new Client([
+            'url' => $baseUrl,
+            'username' => $username,
+            'password' => $password,
+            'serializer' => new JsonApiSerializer(),
+        ]);
+
+        self::assertArrayHasKey('allowedOutputFormats', $camelClient->getProfile());
+        self::assertArrayHasKey('id', $standardClient->getProfile());
+        self::assertSame('user-profile', $jsonApiClient->getProfile()['data']['type']);
     }
 }
